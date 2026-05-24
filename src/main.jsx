cbbc26d07d46ab1fedd4cd76f8025b2fb8ffad7d
Elite Techs <mutavatosh@gmail.com>
2026-05-25 02:17:23 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import EGradeKenya from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <EGradeKenya />
   </React.StrictMode>
 );
