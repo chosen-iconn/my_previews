14ed71a196c488a4f1ed3f901b1cee963a9d8ebd
Elite Techs <mutavatosh@gmail.com>
2026-05-26 11:56:14 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-
 import EGradeKenya from "./App";
-import Teacher from "./Teacher";
-
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<EGradeKenya />} />
-        <Route path="/teacher" element={<Teacher />} />
-      </Routes>
-    </BrowserRouter>
+    <EGradeKenya />
   </React.StrictMode>
 );
